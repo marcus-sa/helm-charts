ad6e4e592dc6d6822354003ef3eac5cc5201fbb3
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:18 -0500

    Add incubator/hoverfly chart

---
+++ repositories/incubator/hoverfly/0.1.0/types.ts
@@ -0,0 +1,21 @@
+// Automatically generated
+
+export interface ChartValues {
+  healthcheckEndpoint?: any
+  hoverflyFlags?: any
+  image?: {
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    adminPort?: any
+    externalAdminPort?: any
+    externalProxyPort?: any
+    proxyPort?: any
+    type?: any
+  }
+}
+
