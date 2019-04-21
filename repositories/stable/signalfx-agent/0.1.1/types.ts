eafd309a040338569c7b425a6b2e7943c7c37074
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:34:11 -0500

    Add stable/signalfx-agent chart

---
+++ repositories/stable/signalfx-agent/0.1.1/types.ts
@@ -0,0 +1,44 @@
+// Automatically generated
+
+export interface ChartValues {
+  agentConfig?: any
+  agentVersion?: any
+  apiServerSkipVerify?: any
+  clusterName?: any
+  containerStatsIntervalSeconds?: any
+  extraPodLabels?: any
+  fullnameOverride?: any
+  gatherClusterMetrics?: any
+  gatherDockerMetrics?: any
+  image?: {
+    pullPolicy?: any
+    pullSecret?: any
+    repository?: any
+    tag?: any
+  }
+  kubeletAPI?: any
+  logLevel?: any
+  metricIntervalSeconds?: any
+  metricNamesToExclude?: any
+  metricsToExclude?: any
+  monitors?: any
+  nameOverride?: any
+  permitReadingSecrets?: any
+  rbac?: {
+    create?: any
+    customRules?: any
+  }
+  readThreads?: any
+  rollingUpdateMaxUnavailable?: any
+  runOnMaster?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  signalFxAccessToken?: any
+  timeout?: any
+  tolerations?: any
+  writeQueueLimitHigh?: any
+  writeQueueLimitLow?: any
+}
+
