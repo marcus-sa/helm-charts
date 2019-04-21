904a4c95bc0ee5f6468c74888f3572b9f75279d8
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:34 -0500

    Add ibm-charts/ibm-messagesight-dev chart

---
+++ repositories/ibm-charts/ibm-messagesight-dev/1.0.1/types.ts
@@ -0,0 +1,16 @@
+// Automatically generated
+
+export interface ChartValues {
+  global?: {
+    virtualIP?: any
+  }
+  messagesight?: {
+    adminPort?: any
+    enabled?: any
+  }
+  messagesightui?: {
+    enabled?: any
+    webuiPort?: any
+  }
+}
+
