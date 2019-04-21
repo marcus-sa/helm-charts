382f21f4a7de327bf51d18d729ffb38dfe436fd5
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:24:39 -0500

    Add bitnami/moodle chart

---
+++ repositories/bitnami/moodle/3.3.1/types.ts
@@ -0,0 +1,82 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  global?: {
+    imageRegistry?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    enabled?: any
+    hosts?: any
+    tls?: any
+  }
+  livenessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  mariadb?: {
+    db?: {
+      name?: any
+      user?: any
+    }
+    enabled?: any
+  }
+  metrics?: {
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    podAnnotations?: any
+    resources?: any
+  }
+  moodleEmail?: any
+  moodlePassword?: any
+  moodleUsername?: any
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    size?: any
+    storageClass?: any
+  }
+  podAnnotations?: any
+  readinessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  resources?: any
+  serviceType?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpProtocol?: any
+  smtpUser?: any
+}
+
