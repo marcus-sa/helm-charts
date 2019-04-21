ad6375b3407dd1a9f266b6618d47697b55cb613e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:33 -0500

    Add ibm-charts/ibm-lsfce-dev chart

---
+++ repositories/ibm-charts/ibm-lsfce-dev/1.0.0/types.ts
@@ -0,0 +1,31 @@
+// Automatically generated
+
+export interface ChartValues {
+  global?: {
+    lsf?: {
+      image?: {
+        pullPolicy?: any
+        repository?: any
+        tag?: any
+      }
+    }
+    storage?: {
+      existingVolumeClaim?: any
+    }
+  }
+  mariadb?: {
+    enableResourceConstraints?: any
+    image?: {
+      repository?: any
+      tag?: any
+    }
+    password?: any
+    resources?: any
+  }
+  master?: {
+    enableResourceConstraints?: any
+    resources?: any
+  }
+  nameOverride?: any
+}
+
