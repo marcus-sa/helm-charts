668340fc7ad29269bdb54de9f8f2160d82714ce7
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:13 -0500

    Add incubator/fluentd chart

---
+++ repositories/incubator/fluentd/0.1.4/types.ts
@@ -0,0 +1,36 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  configMaps?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  output?: {
+    buffer_chunk_limit?: any
+    buffer_queue_limit?: any
+    host?: any
+    port?: any
+  }
+  replicaCount?: any
+  resources?: any
+  service?: {
+    externalPort?: any
+    ports?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
