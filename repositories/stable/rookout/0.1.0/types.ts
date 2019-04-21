7488d02160653c70f97a2feb08deda0644203338
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:44 -0500

    Add stable/rookout chart

---
+++ repositories/stable/rookout/0.1.0/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  listenAll?: any
+  nameOverride?: any
+  replicaCount?: any
+  rookout?: {
+    token?: any
+  }
+  tags?: any
+  token?: any
+}
+
