6ea45b0811045b70b82fea75d3894c1cbfac07c2
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:32 -0500

    Add stable/distributed-jmeter chart

---
+++ repositories/stable/distributed-jmeter/1.0.1/types.ts
@@ -0,0 +1,18 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  master?: {
+    replicaCount?: any
+  }
+  nameOverride?: any
+  server?: {
+    replicaCount?: any
+  }
+}
+
