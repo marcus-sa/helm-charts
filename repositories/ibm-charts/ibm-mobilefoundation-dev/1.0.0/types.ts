463da3b1856e4c24e2892959561ce105294548ad
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:37 -0500

    Add ibm-charts/ibm-mobilefoundation-dev chart

---
+++ repositories/ibm-charts/ibm-mobilefoundation-dev/1.0.0/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  arch?: any
+  helmTestOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  logs?: {
+    consoleFormat?: any
+    consoleLogLevel?: any
+    consoleSource?: any
+  }
+  nameOverride?: any
+  resources?: any
+}
+
