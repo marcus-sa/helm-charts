9dfde0122415d96a490bb38264e43e955b441a14
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:07 -0500

    Add stable/aws-cluster-autoscaler chart

---
+++ repositories/stable/aws-cluster-autoscaler/0.2.0/types.ts
@@ -0,0 +1,31 @@
+// Automatically generated
+
+export interface ChartValues {
+  autoscalingGroups?: any
+  awsRegion?: any
+  extraArgs?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  podAnnotations?: any
+  replicaCount?: any
+  resources?: any
+  scaleDownDelay?: any
+  service?: {
+    annotations?: any
+    clusterIP?: any
+    externalIPs?: any
+    loadBalancerIP?: any
+    loadBalancerSourceRanges?: any
+    servicePort?: any
+    type?: any
+  }
+  skipNodes?: {
+    withLocalStorage?: any
+    withSystemPods?: any
+  }
+}
+
---
+++ repositories/stable/aws-cluster-autoscaler/0.3.0/types.ts
@@ -0,0 +1,32 @@
+// Automatically generated
+
+export interface ChartValues {
+  autoscalingGroups?: any
+  awsRegion?: any
+  extraArgs?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  replicaCount?: any
+  resources?: any
+  scaleDownDelay?: any
+  service?: {
+    annotations?: any
+    clusterIP?: any
+    externalIPs?: any
+    loadBalancerIP?: any
+    loadBalancerSourceRanges?: any
+    servicePort?: any
+    type?: any
+  }
+  skipNodes?: {
+    withLocalStorage?: any
+    withSystemPods?: any
+  }
+}
+
