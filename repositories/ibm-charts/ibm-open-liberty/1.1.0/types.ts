bd274466934b32f3fedd722b52da1fd0e2432d0e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:43 -0500

    Add ibm-charts/ibm-open-liberty chart

---
+++ repositories/ibm-charts/ibm-open-liberty/1.1.0/types.ts
@@ -0,0 +1,41 @@
+// Automatically generated
+
+export interface ChartValues {
+  autoscaling?: {
+    enabled?: any
+    maxReplicas?: any
+    minReplicas?: any
+    targetCPUUtilizationPercentage?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    enabled?: any
+    path?: any
+    rewriteTarget?: any
+  }
+  logs?: {
+    consoleFormat?: any
+    consoleLogLevel?: any
+    consoleSource?: any
+  }
+  nameOverride?: any
+  replicaCount?: any
+  resources?: {
+    constraints?: {
+      enabled?: any
+    }
+    limits?: any
+    requests?: any
+  }
+  service?: {
+    name?: any
+    port?: any
+    targetPort?: any
+    type?: any
+  }
+}
+
---
+++ repositories/ibm-charts/ibm-open-liberty/1.6.0/types.ts
@@ -0,0 +1,24 @@
+// Automatically generated
+
+export interface ChartValues {
+  logs?: {
+    persistLogs?: any
+  }
+  microprofile?: {
+    health?: {
+      enabled?: any
+    }
+  }
+  nameOverride?: any
+  persistence?: {
+    name?: any
+  }
+  service?: {
+    enabled?: any
+    port?: any
+  }
+  ssl?: {
+    enabled?: any
+  }
+}
+
