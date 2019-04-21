1d6c0156adc5c64bf46bb9adeddc5b659cac185f
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:03 -0500

    Add incubator/cockroachdb chart

---
+++ repositories/incubator/cockroachdb/0.1.0/types.ts
@@ -0,0 +1,18 @@
+// Automatically generated
+
+export interface ChartValues {
+  BootstrapImage?: any
+  BootstrapImageTag?: any
+  Component?: any
+  GrpcPort?: any
+  HttpPort?: any
+  Image?: any
+  ImagePullPolicy?: any
+  ImageTag?: any
+  Name?: any
+  Replicas?: any
+  Storage?: any
+  StorageClass?: any
+  resources?: any
+}
+
