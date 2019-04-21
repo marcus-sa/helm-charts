be85bde8d30a030687398d5b43ff23f09cd0543c
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:27 -0500

    Add ibm-charts/ibm-f5bigip-controller chart

---
+++ repositories/ibm-charts/ibm-f5bigip-controller/1.0.0/types.ts
@@ -0,0 +1,24 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  bigIp?: {
+    defaultIngressIp?: any
+    extraArgs?: any
+    namespaces?: any
+    nodeLabelSelector?: any
+    partitionName?: any
+    password?: any
+    poolMemberType?: any
+    url?: any
+    username?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nodeSelector?: any
+  tolerations?: any
+}
+
---
+++ repositories/ibm-charts/ibm-f5bigip-controller/1.1.0/types.ts
@@ -0,0 +1,34 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  bigIp?: {
+    defaultIngressIp?: any
+    extraArgs?: any
+    namespaces?: any
+    nodeLabelSelector?: any
+    partitionName?: any
+    password?: any
+    poolMemberType?: any
+    url?: any
+    username?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nodeSelector?: any
+  resources?: {
+    limits?: {
+      cpu?: any
+      memory?: any
+    }
+    requests?: {
+      cpu?: any
+      memory?: any
+    }
+  }
+  tolerations?: any
+}
+
