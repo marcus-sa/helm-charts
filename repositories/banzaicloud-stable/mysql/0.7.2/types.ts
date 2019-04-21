9bc41a5b3ee459319c826fb2a7b4b61b34d76156
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:42 -0500

    Add banzaicloud-stable/mysql chart

---
+++ repositories/banzaicloud-stable/mysql/0.7.2/types.ts
@@ -0,0 +1,63 @@
+// Automatically generated
+
+export interface ChartValues {
+  configurationFiles?: any
+  fullnameOverride?: any
+  global?: {
+    mysqlPassword?: any
+  }
+  image?: any
+  imagePullPolicy?: any
+  imagePullSecrets?: any
+  imageTag?: any
+  initializationFiles?: any
+  livenessProbe?: {
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  metrics?: {
+    annotations?: any
+    enabled?: any
+    image?: any
+    imagePullPolicy?: any
+    imageTag?: any
+    resources?: any
+  }
+  mysqlAllowEmptyPassword?: any
+  mysqlDatabase?: any
+  mysqlPassword?: any
+  mysqlRootPassword?: any
+  mysqlUser?: any
+  nameOverride?: any
+  nodeSelector?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    size?: any
+    storageClass?: any
+    subPath?: any
+  }
+  readinessProbe?: {
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  resources?: any
+  service?: {
+    nodePort?: any
+    port?: any
+    type?: any
+  }
+  ssl?: {
+    certificates?: any
+    enabled?: any
+    secret?: any
+  }
+}
+
