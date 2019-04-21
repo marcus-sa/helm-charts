00e09d2a41d5e6e0b17720ce273317d5cce5a3cb
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:56 -0500

    Add stable/fluentd-elasticsearch chart

---
+++ repositories/stable/fluentd-elasticsearch/1.5.0/types.ts
@@ -0,0 +1,45 @@
+// Automatically generated
+
+export interface ChartValues {
+  annotations?: any
+  configMaps?: any
+  elasticsearch?: {
+    buffer_chunk_limit?: any
+    buffer_queue_limit?: any
+    host?: any
+    logstash_prefix?: any
+    port?: any
+  }
+  env?: any
+  extraVolumeMounts?: any
+  extraVolumes?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: {
+    enabled?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  podSecurityPolicy?: {
+    annotations?: any
+    enabled?: any
+  }
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  service?: {
+    ports?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+  updateStrategy?: any
+}
+
