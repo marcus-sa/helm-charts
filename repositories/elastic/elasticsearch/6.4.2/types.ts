32a75b435842198cac21871209670a7ddd09e6d2
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:43 -0500

    Add elastic/elasticsearch chart

---
+++ repositories/elastic/elasticsearch/6.4.2/types.ts
@@ -0,0 +1,46 @@
+// Automatically generated
+
+export interface ChartValues {
+  antiAffinity?: any
+  antiAffinityTopologyKey?: any
+  clusterName?: any
+  esJavaOpts?: any
+  extraEnvs?: any
+  fsGroup?: any
+  httpPort?: any
+  image?: any
+  imagePullPolicy?: any
+  imagePullSecrets?: any
+  imageTag?: any
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  masterService?: any
+  maxUnavailable?: any
+  minimumMasterNodes?: any
+  nameOverride?: any
+  networkHost?: any
+  nodeGroup?: any
+  nodeSelector?: any
+  podManagementPolicy?: any
+  readinessProbe?: any
+  replicas?: any
+  resources?: any
+  roles?: {
+    data?: any
+    ingest?: any
+    master?: any
+  }
+  secretMounts?: any
+  sysctlVmMaxMapCount?: any
+  terminationGracePeriod?: any
+  tolerations?: any
+  transportPort?: any
+  updateStrategy?: any
+  volumeClaimTemplate?: any
+}
+
---
+++ repositories/elastic/elasticsearch/6.6.0-alpha1/types.ts
@@ -0,0 +1,50 @@
+// Automatically generated
+
+export interface ChartValues {
+  antiAffinity?: any
+  antiAffinityTopologyKey?: any
+  clusterName?: any
+  esConfig?: any
+  esJavaOpts?: any
+  esMajorVersion?: any
+  extraEnvs?: any
+  fsGroup?: any
+  httpPort?: any
+  image?: any
+  imagePullPolicy?: any
+  imagePullSecrets?: any
+  imageTag?: any
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  initResources?: any
+  masterService?: any
+  maxUnavailable?: any
+  minimumMasterNodes?: any
+  nameOverride?: any
+  networkHost?: any
+  nodeAffinity?: any
+  nodeGroup?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  podManagementPolicy?: any
+  protocol?: any
+  readinessProbe?: any
+  replicas?: any
+  resources?: any
+  roles?: {
+    master?: any
+  }
+  secretMounts?: any
+  sysctlVmMaxMapCount?: any
+  terminationGracePeriod?: any
+  tolerations?: any
+  transportPort?: any
+  updateStrategy?: any
+  volumeClaimTemplate?: any
+}
+
