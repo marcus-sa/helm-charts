cdc85ffa1c301feb12ad9b59a289d93db85a55d1
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:54 -0500

    Add rimusz/gke-pvm-killer chart

---
+++ repositories/rimusz/gke-pvm-killer/0.1.2/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  annotations?: any
+  existingSecret?: any
+  fullnameOverride?: any
+  googleServiceAccount?: any
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: any
+  nameOverride?: any
+  node?: {
+    drainTimeout?: any
+    interval?: any
+  }
+  nodeSelector?: any
+  podDisruptionBudget?: {
+    enabled?: any
+    maxUnavailable?: any
+    minAvailable?: any
+  }
+  rbac?: {
+    enabled?: any
+  }
+  resources?: any
+  securityContext?: {
+    enabled?: any
+    groupId?: any
+    userId?: any
+  }
+  tolerations?: any
+}
+
