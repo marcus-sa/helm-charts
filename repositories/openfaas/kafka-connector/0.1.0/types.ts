e46d2c9aab0de8e17d3b176e6d478b33a62847bc
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:39 -0500

    Add openfaas/kafka-connector chart

---
+++ repositories/openfaas/kafka-connector/0.1.0/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  basic_auth?: any
+  broker_host?: any
+  fullnameOverride?: any
+  gateway_url?: any
+  image?: any
+  nameOverride?: any
+  nodeSelector?: any
+  print_response?: any
+  rebuild_interval?: any
+  replicas?: any
+  tolerations?: any
+  topics?: any
+  upstream_timeout?: any
+}
+
---
+++ repositories/openfaas/kafka-connector/0.2.2/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  basic_auth?: any
+  broker_host?: any
+  fullnameOverride?: any
+  gateway_url?: any
+  image?: any
+  nameOverride?: any
+  nodeSelector?: any
+  print_response?: any
+  print_response_body?: any
+  rebuild_interval?: any
+  replicas?: any
+  tolerations?: any
+  topics?: any
+  upstream_timeout?: any
+}
+
