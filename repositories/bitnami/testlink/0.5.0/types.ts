9f0ee60402ea1761212a1c8c40a7d10750382c89
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:31 -0500

    Add bitnami/testlink chart

---
+++ repositories/bitnami/testlink/0.5.0/types.ts
@@ -0,0 +1,46 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  image?: any
+  imagePullPolicy?: any
+  mariadb?: {
+    enabled?: any
+    mariadbDatabase?: any
+    mariadbUser?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    apache?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+    enabled?: any
+    testlink?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  resources?: any
+  serviceType?: any
+  smtpConnectionMode?: any
+  smtpEnable?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpUser?: any
+  testlinkEmail?: any
+  testlinkLanguage?: any
+  testlinkPassword?: any
+  testlinkUsername?: any
+}
+
---
+++ repositories/bitnami/testlink/4.2.0/types.ts
@@ -0,0 +1,81 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  global?: {
+    imagePullSecrets?: any
+    imageRegistry?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  mariadb?: {
+    db?: {
+      name?: any
+      user?: any
+    }
+    enabled?: any
+  }
+  metrics?: {
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      pullSecrets?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    podAnnotations?: any
+    resources?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  persistence?: {
+    apache?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+    enabled?: any
+    testlink?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  podAnnotations?: any
+  resources?: any
+  service?: {
+    externalTrafficPolicy?: any
+    httpsPort?: any
+    loadBalancerIP?: any
+    nodePorts?: {
+      http?: any
+      https?: any
+    }
+    port?: any
+    type?: any
+  }
+  smtpConnectionMode?: any
+  smtpEnable?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpUser?: any
+  testlinkEmail?: any
+  testlinkLanguage?: any
+  testlinkPassword?: any
+  testlinkUsername?: any
+}
+
