d3fdb7f6d0bb3a3157a9590e80075e75c553f3a6
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:22 -0500

    Add ibm-charts/ibm-csi-nfs chart

---
+++ repositories/ibm-charts/ibm-csi-nfs/0.2.1/types.ts
@@ -0,0 +1,21 @@
+// Automatically generated
+
+export interface ChartValues {
+  attacherImage?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nfspluginImage?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  registerImage?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+}
+
