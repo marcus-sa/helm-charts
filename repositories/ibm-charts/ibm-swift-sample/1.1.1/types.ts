72c13ca01362584e31fc1eadbda6ccdf4116483f
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:50 -0500

    Add ibm-charts/ibm-swift-sample chart

---
+++ repositories/ibm-charts/ibm-swift-sample/1.1.1/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  arch?: any
+  hpa?: {
+    enabled?: any
+    maxReplicas?: any
+    metrics?: {
+      cpu?: {
+        targetAverageUtilization?: any
+      }
+    }
+    minReplicas?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    resources?: {
+      requests?: {
+        cpu?: any
+        memory?: any
+      }
+    }
+    tag?: any
+  }
+  livenessProbe?: {
+    initialDelaySeconds?: any
+    periodSeconds?: any
+  }
+  nameOverride?: any
+  replicaCount?: any
+  revisionHistoryLimit?: any
+  service?: {
+    servicePort?: any
+    type?: any
+  }
+}
+
