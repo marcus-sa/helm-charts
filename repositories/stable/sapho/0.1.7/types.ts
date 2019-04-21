20badb939fd087c1c86b8f102024d8a1a775edeb
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:46 -0500

    Add stable/sapho chart

---
+++ repositories/stable/sapho/0.1.7/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: any
+  imagePullPolicy?: any
+  nameOverride?: any
+  replicaCount?: any
+  resources?: any
+  saphoDBport?: any
+  saphoDBtype?: any
+  saphoDBuser?: any
+  service?: {
+    externalPort?: any
+    internalPort?: any
+    name?: any
+    type?: any
+  }
+}
+
---
+++ repositories/stable/sapho/0.2.1/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: any
+  imagePullPolicy?: any
+  nameOverride?: any
+  replicaCount?: any
+  resources?: any
+  saphoDBport?: any
+  saphoDBtype?: any
+  saphoDBuser?: any
+  saphoVersion?: any
+  service?: {
+    externalPort?: any
+    internalPort?: any
+    name?: any
+    type?: any
+  }
+}
+
