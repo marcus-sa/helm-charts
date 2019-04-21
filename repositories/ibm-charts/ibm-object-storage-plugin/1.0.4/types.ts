32639e12cb0cec590fefd32e40aaf1cb11f7a36b
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:42 -0500

    Add ibm-charts/ibm-object-storage-plugin chart

---
+++ repositories/ibm-charts/ibm-object-storage-plugin/1.0.4/types.ts
@@ -0,0 +1,29 @@
+// Automatically generated
+
+export interface ChartValues {
+  cos?: {
+    endpoint?: any
+    storageClass?: any
+  }
+  dcname?: any
+  iamEndpoint?: any
+  image?: {
+    driverBuild?: any
+    driverImage?: {
+      ibmContainerRegistry?: any
+      publicRegistry?: any
+    }
+    pluginBuild?: any
+    pluginImage?: {
+      ibmContainerRegistry?: any
+      publicRegistry?: any
+    }
+    pullPolicy?: any
+  }
+  maxUnavailableNodeCount?: any
+  nameOverride?: any
+  replicaCount?: any
+  secondaryValueFile?: any
+  useCustomPSP?: any
+}
+
