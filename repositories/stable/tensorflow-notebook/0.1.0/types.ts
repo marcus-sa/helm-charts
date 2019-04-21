310bc704ad90ebda43f5f943421b6aa5dafaeae2
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:35:07 -0500

    Add stable/tensorflow-notebook chart

---
+++ repositories/stable/tensorflow-notebook/0.1.0/types.ts
@@ -0,0 +1,25 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  jupyter?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    password?: any
+  }
+  nameOverride?: any
+  service?: {
+    type?: any
+  }
+  tensorboard?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+}
+
