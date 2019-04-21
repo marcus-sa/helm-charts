d453c994dcc944139ca57466751bf26248d9bbe7
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:24 -0500

    Add bitnami/sugarcrm chart

---
+++ repositories/bitnami/sugarcrm/0.2.4/types.ts
@@ -0,0 +1,36 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: any
+  imagePullPolicy?: any
+  mariadb?: {
+    mariadbRootPassword?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    apache?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+    enabled?: any
+    sugarcrm?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  resources?: any
+  serviceType?: any
+  sugarcrmEmail?: any
+  sugarcrmLastName?: any
+  sugarcrmLoadBalancerIP?: any
+  sugarcrmPassword?: any
+  sugarcrmSmtpHost?: any
+  sugarcrmSmtpPassword?: any
+  sugarcrmSmtpPort?: any
+  sugarcrmSmtpProtocol?: any
+  sugarcrmSmtpUser?: any
+  sugarcrmUsername?: any
+}
+
---
+++ repositories/bitnami/sugarcrm/0.3.1/types.ts
@@ -0,0 +1,47 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  image?: any
+  imagePullPolicy?: any
+  mariadb?: {
+    enabled?: any
+    mariadbDatabase?: any
+    mariadbRootPassword?: any
+    mariadbUser?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    apache?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+    enabled?: any
+    sugarcrm?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  resources?: any
+  serviceType?: any
+  sugarcrmEmail?: any
+  sugarcrmLastName?: any
+  sugarcrmLoadBalancerIP?: any
+  sugarcrmPassword?: any
+  sugarcrmSmtpHost?: any
+  sugarcrmSmtpPassword?: any
+  sugarcrmSmtpPort?: any
+  sugarcrmSmtpProtocol?: any
+  sugarcrmSmtpUser?: any
+  sugarcrmUsername?: any
+}
+
