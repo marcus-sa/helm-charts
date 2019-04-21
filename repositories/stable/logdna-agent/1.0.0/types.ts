0534a465520dbdbd5e99d86fe6ae956cb3b0b9ae
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:31:06 -0500

    Add stable/logdna-agent chart

---
+++ repositories/stable/logdna-agent/1.0.0/types.ts
@@ -0,0 +1,21 @@
+// Automatically generated
+
+export interface ChartValues {
+  daemonset?: {
+    tolerations?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    name?: any
+    pullPolicy?: any
+    tag?: any
+  }
+  logdna?: {
+    autoupdate?: any
+    key?: any
+    tags?: any
+  }
+  nameOverride?: any
+  resources?: any
+}
+
