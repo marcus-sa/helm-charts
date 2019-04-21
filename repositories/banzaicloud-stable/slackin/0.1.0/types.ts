a403dd69fd3cdeda60ffbaa33eca4bb1679d9fc6
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:46 -0500

    Add banzaicloud-stable/slackin chart

---
+++ repositories/banzaicloud-stable/slackin/0.1.0/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  googleCaptchaSecret?: any
+  googleCaptchaSiteKey?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    port?: any
+    type?: any
+  }
+  slackApiToken?: any
+  slackSubdomain?: any
+  slackinCSS?: any
+  slackinChannels?: any
+  slackinCoc?: any
+  slackinHostname?: any
+  slackinPort?: any
+  slackinTheme?: any
+  tolerations?: any
+}
+
