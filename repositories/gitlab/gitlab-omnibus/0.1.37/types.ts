c239861e92c3e15a3bd73958ddd6f6e65d83b1d3
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:13 -0500

    Add gitlab/gitlab-omnibus chart

---
+++ repositories/gitlab/gitlab-omnibus/0.1.37/types.ts
@@ -0,0 +1,44 @@
+// Automatically generated
+
+export interface ChartValues {
+  baseDomain?: any
+  baseIP?: any
+  gitlab?: any
+  gitlabCEImage?: any
+  gitlabConfigAccessMode?: any
+  gitlabConfigStorageClass?: any
+  gitlabConfigStorageSize?: any
+  gitlabDataAccessMode?: any
+  gitlabDataStorageClass?: any
+  gitlabDataStorageSize?: any
+  gitlabEEImage?: any
+  gitlabEELicense?: any
+  gitlabRailsStorageSize?: any
+  gitlabRegistryAccessMode?: any
+  gitlabRegistryStorageClass?: any
+  gitlabRegistryStorageSize?: any
+  gitlabUrl?: any
+  healthCheckToken?: any
+  initialSharedRunnersRegistrationToken?: any
+  legoEmail?: any
+  mattermostAppSecret?: any
+  mattermostAppUID?: any
+  nameOverride?: any
+  omnibusConfigRuby?: any
+  pagesExternalDomain?: any
+  pagesExternalScheme?: any
+  pagesTlsSecret?: any
+  postgresAccessMode?: any
+  postgresDedicatedStorage?: any
+  postgresImage?: any
+  postgresPassword?: any
+  postgresStorageClass?: any
+  postgresStorageSize?: any
+  provider?: any
+  redisAccessMode?: any
+  redisDedicatedStorage?: any
+  redisImage?: any
+  redisStorageClass?: any
+  redisStorageSize?: any
+}
+
