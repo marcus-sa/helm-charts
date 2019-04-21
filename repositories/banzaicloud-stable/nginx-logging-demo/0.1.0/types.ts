4f42b585f3b8b4352593f22c33e4ee909f086476
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:43 -0500

    Add banzaicloud-stable/nginx-logging-demo chart

---
+++ repositories/banzaicloud-stable/nginx-logging-demo/0.1.0/types.ts
@@ -0,0 +1,27 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
