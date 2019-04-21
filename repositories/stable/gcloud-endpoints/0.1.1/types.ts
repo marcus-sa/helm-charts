63b4cc910500cdee43bf0589ca1b6ba25e100552
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:59 -0500

    Add stable/gcloud-endpoints chart

---
+++ repositories/stable/gcloud-endpoints/0.1.1/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  backend?: any
+  http2Port?: any
+  httpPort?: any
+  image?: any
+  imagePullPolicy?: any
+  nameOverride?: any
+  nginxConfig?: any
+  resources?: any
+  service?: any
+  serviceAccountKey?: any
+  serviceType?: any
+  sslPort?: any
+  statusPort?: any
+  version?: any
+}
+
