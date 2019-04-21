929e66d9334e8e3f170c56eb532e50cd4cad247c
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:57 -0500

    Add stable/kubed chart

---
+++ repositories/stable/kubed/0.1.0/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  criticalAddon?: any
+  imagePullPolicy?: any
+  imagePullSecrets?: any
+  kubed?: {
+    image?: any
+    tag?: any
+  }
+  logLevel?: any
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+    serviceAccountName?: any
+  }
+  replicaCount?: any
+}
+
