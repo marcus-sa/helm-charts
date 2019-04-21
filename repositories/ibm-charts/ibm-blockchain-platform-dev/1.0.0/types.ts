df2cee09820b0765f4fa666a4adfe3c1a0f09652
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:19 -0500

    Add ibm-charts/ibm-blockchain-platform-dev chart

---
+++ repositories/ibm-charts/ibm-blockchain-platform-dev/1.0.0/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  ca?: {
+    app?: {
+      arch?: any
+    }
+    enabled?: any
+    service?: {
+      type?: any
+    }
+  }
+  global?: {
+    multiarch?: any
+    productID?: any
+    productName?: any
+    serviceAccountName?: any
+  }
+  orderer?: {
+    app?: {
+      arch?: any
+    }
+    enabled?: any
+    service?: {
+      type?: any
+    }
+  }
+  peer?: {
+    app?: {
+      arch?: any
+    }
+    enabled?: any
+    service?: {
+      type?: any
+    }
+  }
+}
+
---
+++ repositories/ibm-charts/ibm-blockchain-platform-dev/1.0.2/types.ts
@@ -0,0 +1,37 @@
+// Automatically generated
+
+export interface ChartValues {
+  ca?: {
+    app?: {
+      arch?: any
+    }
+    enabled?: any
+    image?: {
+      initimage?: any
+      inittag?: any
+    }
+    service?: {
+      type?: any
+    }
+  }
+  global?: {
+    multiarch?: any
+    serviceAccountName?: any
+  }
+  orderer?: {
+    enabled?: any
+    service?: {
+      type?: any
+    }
+  }
+  peer?: {
+    app?: {
+      arch?: any
+    }
+    enabled?: any
+    service?: {
+      type?: any
+    }
+  }
+}
+
