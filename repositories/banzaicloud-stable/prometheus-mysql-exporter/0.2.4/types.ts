b012caf7b0f7d766100f39f0d1c945cae44d090e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:45 -0500

    Add banzaicloud-stable/prometheus-mysql-exporter chart

---
+++ repositories/banzaicloud-stable/prometheus-mysql-exporter/0.2.4/types.ts
@@ -0,0 +1,50 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  annotations?: any
+  cloudsqlproxy?: {
+    credentials?: any
+    enabled?: any
+    existingSecret?: any
+    existingSecretKey?: any
+    image?: {
+      PullPolicy?: any
+      repo?: any
+      tag?: any
+    }
+    instanceConnectionName?: any
+    port?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  mysql?: {
+    db?: any
+    host?: any
+    param?: any
+    pass?: any
+    port?: any
+    protocol?: any
+    user?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    externalPort?: any
+    internalPort?: any
+    name?: any
+    type?: any
+  }
+  serviceMonitor?: {
+    additionalLabels?: any
+    enabled?: any
+  }
+  tolerations?: any
+}
+
