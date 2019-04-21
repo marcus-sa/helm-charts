97f70d3eba4c2d853e2a5d654c90b371bf87baec
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:04 -0500

    Add incubator/consul chart

---
+++ repositories/incubator/consul/0.1.2/types.ts
@@ -0,0 +1,22 @@
+// Automatically generated
+
+export interface ChartValues {
+  Component?: any
+  ConsulDnsPort?: any
+  Cpu?: any
+  HttpPort?: any
+  Image?: any
+  ImagePullPolicy?: any
+  ImageTag?: any
+  Memory?: any
+  Name?: any
+  Replicas?: any
+  RpcPort?: any
+  SerflanPort?: any
+  SerflanUdpPort?: any
+  SerfwanPort?: any
+  SerfwanUdpPort?: any
+  ServerPort?: any
+  Storage?: any
+}
+
