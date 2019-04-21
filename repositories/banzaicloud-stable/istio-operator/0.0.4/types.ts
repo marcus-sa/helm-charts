0d3b1723c5ec0b109e05b0086fcc315604291b8e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:41 -0500

    Add banzaicloud-stable/istio-operator chart

---
+++ repositories/banzaicloud-stable/istio-operator/0.0.4/types.ts
@@ -0,0 +1,32 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  nameOverride?: any
+  nodeSelector?: any
+  operator?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    resources?: any
+  }
+  prometheusMetrics?: {
+    authProxy?: {
+      enabled?: any
+      image?: {
+        pullPolicy?: any
+        repository?: any
+        tag?: any
+      }
+    }
+    enabled?: any
+  }
+  rbac?: {
+    enabled?: any
+  }
+  tolerations?: any
+}
+
