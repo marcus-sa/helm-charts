5ea75905733c069f2c52c010ec5cf07114ea4f9e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:37 -0500

    Add incubator/mongodb-replicaset chart

---
+++ repositories/incubator/mongodb-replicaset/0.1.4/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  ClusterRole?: any
+  Component?: any
+  Cpu?: any
+  Image?: any
+  ImagePullPolicy?: any
+  ImageTag?: any
+  InstallImage?: any
+  InstallImageTag?: any
+  Memory?: any
+  Name?: any
+  PeerPort?: any
+  ReplicaSet?: any
+  Replicas?: any
+  Storage?: any
+  StorageClass?: any
+}
+
