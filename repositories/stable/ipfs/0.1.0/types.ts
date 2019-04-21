3a6af6a050241340a50487f0edc42fe98a0729af
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:29 -0500

    Add stable/ipfs chart

---
+++ repositories/stable/ipfs/0.1.0/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: any
+  nameOverride?: any
+  persistence?: {
+    accessModes?: any
+    annotations?: any
+    enabled?: any
+    size?: any
+    storageClass?: any
+  }
+  replicaCount?: any
+  resources?: any
+  service?: {
+    nameOverride?: any
+    type?: any
+  }
+}
+
