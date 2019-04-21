b8fdf857a169273e0587ec92d4cf5150da3c775c
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:35:09 -0500

    Add stable/tensorflow-serving chart

---
+++ repositories/stable/tensorflow-serving/0.1.0/types.ts
@@ -0,0 +1,30 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  gpuCount?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  modelBasePath?: any
+  modelName?: any
+  mountPath?: any
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    matchLabels?: any
+    pvc?: {
+      matchLabels?: any
+    }
+    size?: any
+    storageClass?: any
+  }
+  replicas?: any
+  resources?: any
+  serviceType?: any
+}
+
