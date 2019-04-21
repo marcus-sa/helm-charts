3a87a9c6db40e49b0554c86ca98a957aad3be590
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:30 -0500

    Add stable/jaeger-operator chart

---
+++ repositories/stable/jaeger-operator/2.4.1/types.ts
@@ -0,0 +1,23 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
