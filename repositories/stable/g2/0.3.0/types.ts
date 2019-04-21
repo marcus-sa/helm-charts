22645014e869f2f1e60c2fc81a47443dfc909d5a
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:57 -0500

    Add stable/g2 chart

---
+++ repositories/stable/g2/0.3.0/types.ts
@@ -0,0 +1,17 @@
+// Automatically generated
+
+export interface ChartValues {
+  g2?: {
+    image?: any
+    pullPolicy?: any
+    serviceType?: any
+    tag?: any
+  }
+  nameOverride?: any
+  rbac?: {
+    apiVersion?: any
+    install?: any
+  }
+  replicaCount?: any
+}
+
