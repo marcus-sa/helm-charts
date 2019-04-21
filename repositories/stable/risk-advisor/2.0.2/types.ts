158f7842ea487d8686c2eb13299e97adc424501b
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:39 -0500

    Add stable/risk-advisor chart

---
+++ repositories/stable/risk-advisor/2.0.2/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    nodePort?: any
+    port?: any
+    targetPort?: any
+    type?: any
+  }
+}
+
