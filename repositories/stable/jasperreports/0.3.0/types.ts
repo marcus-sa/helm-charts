3b2340912e64a6ad7b50ee687bbb70872939ffb8
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:32 -0500

    Add stable/jasperreports chart

---
+++ repositories/stable/jasperreports/0.3.0/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  image?: any
+  imagePullPolicy?: any
+  jasperreportsEmail?: any
+  jasperreportsPassword?: any
+  jasperreportsUsername?: any
+  mariadb?: {
+    enabled?: any
+    mariadbDatabase?: any
+    mariadbUser?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  serviceType?: any
+  smtpEmail?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpProtocol?: any
+  smtpUser?: any
+}
+
---
+++ repositories/stable/jasperreports/1.0.4/types.ts
@@ -0,0 +1,43 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  jasperreportsEmail?: any
+  jasperreportsPassword?: any
+  jasperreportsUsername?: any
+  mariadb?: {
+    enabled?: any
+    mariadbDatabase?: any
+    mariadbUser?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  serviceType?: any
+  smtpEmail?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpProtocol?: any
+  smtpUser?: any
+}
+
