5043d8d33d4d3aa302e1534327d22b7a6dac3b0b
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:12 -0500

    Add stable/graphite chart

---
+++ repositories/stable/graphite/0.1.0/types.ts
@@ -0,0 +1,36 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  configMaps?: any
+  fullnameOverride?: any
+  image?: {
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  service?: {
+    annotations?: any
+    labels?: any
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
---
+++ repositories/stable/graphite/0.2.0/types.ts
@@ -0,0 +1,40 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  configMaps?: any
+  fullnameOverride?: any
+  image?: {
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  service?: {
+    annotations?: any
+    labels?: any
+    port?: any
+    type?: any
+  }
+  statsd?: {
+    interface?: any
+  }
+  statsdConfigMaps?: any
+  tolerations?: any
+}
+
