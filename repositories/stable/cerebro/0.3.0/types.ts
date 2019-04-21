608e735e53ad33d37f6584facd35009cf81269f7
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:11 -0500

    Add stable/cerebro chart

---
+++ repositories/stable/cerebro/0.3.0/types.ts
@@ -0,0 +1,35 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  config?: {
+    basePath?: any
+    hosts?: any
+    restHistorySize?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    annotations?: any
+    labels?: any
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
---
+++ repositories/stable/cerebro/0.5.0/types.ts
@@ -0,0 +1,44 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  config?: {
+    basePath?: any
+    hosts?: any
+    restHistorySize?: any
+    secret?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  init?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  revisionHistoryLimit?: any
+  service?: {
+    annotations?: any
+    labels?: any
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
