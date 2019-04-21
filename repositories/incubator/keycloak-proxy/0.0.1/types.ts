b968a598ed251badfb85f29368db2eefa75f04c8
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:31 -0500

    Add incubator/keycloak-proxy chart

---
+++ repositories/incubator/keycloak-proxy/0.0.1/types.ts
@@ -0,0 +1,39 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  configmap?: {
+    authServerUrl?: any
+    pattern?: any
+    realm?: any
+    realmPublicKey?: any
+    resource?: any
+    rolesAllowed?: any
+    secret?: any
+    targetUrl?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    nodePort?: any
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
