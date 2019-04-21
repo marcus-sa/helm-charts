450e5ddd244a08a46d4016cbde48506334a0bd17
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:35:26 -0500

    Add stable/unbound chart

---
+++ repositories/stable/unbound/0.1.2/types.ts
@@ -0,0 +1,34 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  allowedIpRanges?: any
+  forwardZones?: any
+  fullnameOverride?: any
+  healthz?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  localRecords?: any
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  tolerations?: any
+  unbound?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    numThreads?: any
+    serverPort?: any
+    statsCumulative?: any
+    statsInterval?: any
+    verbosity?: any
+  }
+}
+
---
+++ repositories/stable/unbound/1.0.0/types.ts
@@ -0,0 +1,37 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  allowedIpRanges?: any
+  externalIP?: any
+  forwardZones?: any
+  fullnameOverride?: any
+  healthz?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  localRecords?: any
+  localZones?: any
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  stubZones?: any
+  tolerations?: any
+  unbound?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    numThreads?: any
+    serverPort?: any
+    statsCumulative?: any
+    statsInterval?: any
+    verbosity?: any
+  }
+}
+
