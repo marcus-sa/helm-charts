b0faca6008dac984d8054ebf027c3b8819f364b6
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:35 -0500

    Add stable/reloader chart

---
+++ repositories/stable/reloader/1.0.2/types.ts
@@ -0,0 +1,33 @@
+// Automatically generated
+
+export interface ChartValues {
+  nameOverride?: any
+  reloader?: {
+    deployment?: {
+      annotations?: any
+      env?: {
+        field?: any
+        open?: any
+        secret?: any
+      }
+      image?: {
+        name?: any
+        pullPolicy?: any
+        tag?: any
+      }
+      labels?: any
+    }
+    matchLabels?: any
+    rbac?: {
+      enabled?: any
+      labels?: any
+    }
+    serviceAccount?: {
+      create?: any
+      labels?: any
+      name?: any
+    }
+    watchGlobally?: any
+  }
+}
+
