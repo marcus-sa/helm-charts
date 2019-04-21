1d8f87a4fa71811ca3b9e178ad47cc9f828aaadd
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:29 -0500

    Add ibm-charts/ibm-integration-bus-dev chart

---
+++ repositories/ibm-charts/ibm-integration-bus-dev/0.1.0/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  license?: any
+  nameOverride?: any
+  nodename?: any
+  servername?: any
+  service?: {
+    nodelistenerPort?: any
+    serverlistenerPort?: any
+    type?: any
+    webuiPort?: any
+  }
+}
+
---
+++ repositories/ibm-charts/ibm-integration-bus-dev/1.0.1/types.ts
@@ -0,0 +1,26 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: {
+    pullPolicy?: any
+    pullSecret?: any
+    repository?: any
+    tag?: any
+  }
+  license?: any
+  nameOverride?: any
+  nodename?: any
+  resources?: {
+    limits?: any
+    requests?: any
+  }
+  servername?: any
+  service?: {
+    nodelistenerPort?: any
+    replicas?: any
+    serverlistenerPort?: any
+    type?: any
+    webuiPort?: any
+  }
+}
+
