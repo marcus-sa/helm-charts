b0f190fc6826850f36d8231f719944a3b0a76c40
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:03 -0500

    Add incubator/common chart

---
+++ repositories/incubator/common/0.0.2/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  fullnamePrefix?: any
+  fullnameSuffix?: any
+  global?: {
+    fullnamePrefix?: any
+    fullnameSuffix?: any
+    namePrefix?: any
+    nameSuffix?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    hosts?: any
+    tls?: any
+  }
+  nameOverride?: any
+  namePrefix?: any
+  nameSuffix?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  service?: {
+    type?: any
+  }
+}
+
