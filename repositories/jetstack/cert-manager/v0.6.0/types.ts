35cd801008e3fcb27869309b54b035cd39b91de2
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:55 -0500

    Add jetstack/cert-manager chart

---
+++ repositories/jetstack/cert-manager/v0.6.0/types.ts
@@ -0,0 +1,48 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  clusterResourceNamespace?: any
+  extraArgs?: any
+  extraEnv?: any
+  fullnameOverride?: any
+  global?: {
+    imagePullSecrets?: any
+    priorityClassName?: any
+  }
+  http_proxy?: any
+  https_proxy?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingressShim?: any
+  leaderElection?: {
+    namespace?: any
+  }
+  nameOverride?: any
+  no_proxy?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  podDnsConfig?: any
+  podDnsPolicy?: any
+  podLabels?: any
+  rbac?: {
+    create?: any
+  }
+  replicaCount?: any
+  resources?: any
+  securityContext?: {
+    enabled?: any
+    fsGroup?: any
+    runAsUser?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  strategy?: any
+  tolerations?: any
+}
+
---
+++ repositories/jetstack/cert-manager/v0.7.0/types.ts
@@ -0,0 +1,53 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  clusterResourceNamespace?: any
+  extraArgs?: any
+  extraEnv?: any
+  fullnameOverride?: any
+  globa?: {
+    leaderElection?: {
+      namespace?: any
+    }
+  }
+  global?: {
+    imagePullSecrets?: any
+    leaderElection?: {
+      namespace?: any
+    }
+    priorityClassName?: any
+    rbac?: {
+      create?: any
+    }
+  }
+  http_proxy?: any
+  https_proxy?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingressShim?: any
+  nameOverride?: any
+  no_proxy?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  podDnsConfig?: any
+  podDnsPolicy?: any
+  podLabels?: any
+  replicaCount?: any
+  resources?: any
+  securityContext?: {
+    enabled?: any
+    fsGroup?: any
+    runAsUser?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  strategy?: any
+  tolerations?: any
+}
+
