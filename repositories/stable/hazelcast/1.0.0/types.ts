6ea0d15c3b30447f06a9e4001eabd2b307293960
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:15 -0500

    Add stable/hazelcast chart

---
+++ repositories/stable/hazelcast/1.0.0/types.ts
@@ -0,0 +1,51 @@
+// Automatically generated
+
+export interface ChartValues {
+  cluster?: {
+    memberCount?: any
+  }
+  fullnameOverride?: any
+  hazelcast?: {
+    configurationFiles?: any
+    javaOpts?: any
+    rest?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  readinessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  resources?: any
+  service?: {
+    clusterIP?: any
+    port?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+}
+
---
+++ repositories/stable/hazelcast/1.2.0/types.ts
@@ -0,0 +1,56 @@
+// Automatically generated
+
+export interface ChartValues {
+  cluster?: {
+    memberCount?: any
+  }
+  fullnameOverride?: any
+  gracefulShutdown?: {
+    enabled?: any
+    maxWaitSeconds?: any
+  }
+  hazelcast?: {
+    configurationFiles?: any
+    javaOpts?: any
+    rest?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  readinessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  resources?: any
+  securityContext?: any
+  service?: {
+    clusterIP?: any
+    port?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+}
+
