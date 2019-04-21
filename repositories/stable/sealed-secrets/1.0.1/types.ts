b2657856087229701d0ccbf353670a63f4cacce0
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:52 -0500

    Add stable/sealed-secrets chart

---
+++ repositories/stable/sealed-secrets/1.0.1/types.ts
@@ -0,0 +1,27 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  crd?: {
+    keep?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  secretName?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
---
+++ repositories/stable/sealed-secrets/1.0.2/types.ts
@@ -0,0 +1,28 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  crd?: {
+    create?: any
+    keep?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  secretName?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
