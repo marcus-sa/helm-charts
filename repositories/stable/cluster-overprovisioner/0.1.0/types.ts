c75910e40c5bd0d64481bce1077550bb44dd3459
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:17 -0500

    Add stable/cluster-overprovisioner chart

---
+++ repositories/stable/cluster-overprovisioner/0.1.0/types.ts
@@ -0,0 +1,25 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  priorityClassDefault?: {
+    name?: any
+    value?: any
+  }
+  priorityClassOverprovision?: {
+    name?: any
+    value?: any
+  }
+  replicaCount?: any
+  resources?: any
+  tolerations?: any
+}
+
---
+++ repositories/stable/cluster-overprovisioner/0.2.0/types.ts
@@ -0,0 +1,26 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  priorityClassDefault?: {
+    enabled?: any
+    name?: any
+    value?: any
+  }
+  priorityClassOverprovision?: {
+    name?: any
+    value?: any
+  }
+  replicaCount?: any
+  resources?: any
+  tolerations?: any
+}
+
