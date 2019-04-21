d88ec28e7f79a6496941b22cf852ed8391deaeed
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:33:15 -0500

    Add stable/prometheus-to-sd chart

---
+++ repositories/stable/prometheus-to-sd/0.1.0/types.ts
@@ -0,0 +1,16 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  metricsSources?: any
+  nameOverride?: any
+  nodeSelector?: any
+  port?: any
+  replicaCount?: any
+  resources?: any
+}
+
