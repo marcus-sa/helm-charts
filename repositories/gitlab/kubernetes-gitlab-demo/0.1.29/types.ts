b438802fb8e726b3fb4b80cda142899ca13af41a
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:15 -0500

    Add gitlab/kubernetes-gitlab-demo chart

---
+++ repositories/gitlab/kubernetes-gitlab-demo/0.1.29/types.ts
@@ -0,0 +1,29 @@
+// Automatically generated
+
+export interface ChartValues {
+  baseDomain?: any
+  baseIP?: any
+  gitlab?: any
+  gitlabCEImage?: any
+  gitlabConfigStorageSize?: any
+  gitlabEEImage?: any
+  gitlabEELicense?: any
+  gitlabNamespace?: any
+  gitlabRailsStorageSize?: any
+  gitlabRegistryStorageSize?: any
+  healthCheckToken?: any
+  initialSharedRunnersRegistrationToken?: any
+  legoEmail?: any
+  mattermostAppSecret?: any
+  mattermostAppUID?: any
+  nameOverride?: any
+  postgresDedicatedStorage?: any
+  postgresImage?: any
+  postgresPassword?: any
+  postgresStorageSize?: any
+  provider?: any
+  redisDedicatedStorage?: any
+  redisImage?: any
+  redisStorageSize?: any
+}
+
