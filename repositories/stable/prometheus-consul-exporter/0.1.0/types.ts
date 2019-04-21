8471bf584058a0725eeaf68f8b7830b796694b3f
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:32:47 -0500

    Add stable/prometheus-consul-exporter chart

---
+++ repositories/stable/prometheus-consul-exporter/0.1.0/types.ts
@@ -0,0 +1,37 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  consulServer?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  options?: any
+  rbac?: {
+    create?: any
+    pspEnabled?: any
+  }
+  replicaCount?: any
+  resources?: any
+  service?: {
+    port?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
---
+++ repositories/stable/prometheus-consul-exporter/0.1.3/types.ts
@@ -0,0 +1,39 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  consulServer?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  options?: any
+  rbac?: {
+    create?: any
+    pspEnabled?: any
+    pspUseAppArmor?: any
+  }
+  replicaCount?: any
+  resources?: any
+  service?: {
+    annotations?: any
+    port?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
