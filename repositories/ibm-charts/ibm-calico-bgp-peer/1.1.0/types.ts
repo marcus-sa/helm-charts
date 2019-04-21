772ec71a6f663b3723dabcce5d12705ad4db3935
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:20 -0500

    Add ibm-charts/ibm-calico-bgp-peer chart

---
+++ repositories/ibm-charts/ibm-calico-bgp-peer/1.1.0/types.ts
@@ -0,0 +1,18 @@
+// Automatically generated
+
+export interface ChartValues {
+  arch?: any
+  asNumber?: any
+  etcd?: {
+    endpoint?: any
+    secret?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  node?: any
+  peerIp?: any
+}
+
