089d19401d65d7ff5ab2655c1e5821510440c721
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:10 -0500

    Add jfrog/kubexray chart

---
+++ repositories/jfrog/kubexray/0.3.3/types.ts
@@ -0,0 +1,68 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  env?: {
+    logLevel?: any
+  }
+  existingSecret?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  imagePullSecrets?: any
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  livenessProbe?: any
+  nameOverride?: any
+  nodeSelector?: any
+  podDisruptionBudget?: {
+    enabled?: any
+    maxUnavailable?: any
+    minAvailable?: any
+  }
+  rbac?: {
+    enabled?: any
+  }
+  readinessProbe?: any
+  replicaCount?: any
+  resources?: any
+  scanPolicy?: {
+    license?: {
+      deployments?: any
+      statefulSets?: any
+      whitelistNamespaces?: any
+    }
+    security?: {
+      deployments?: any
+      statefulSets?: any
+      whitelistNamespaces?: any
+    }
+    unscanned?: {
+      deployments?: any
+      statefulSets?: any
+      whitelistNamespaces?: any
+    }
+  }
+  securityContext?: {
+    enabled?: any
+    kubeXrayGroupId?: any
+    kubeXrayUserId?: any
+  }
+  service?: {
+    externalTrafficPolicy?: any
+    loadBalancerIP?: any
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+  xrayConfig?: any
+}
+
---
+++ repositories/jfrog/kubexray/0.3.5/chart/CHANGELOG.md
@@ -0,0 +1,20 @@
+# JFrog KubeXray Chart Changelog
+All changes to this chart will be documented in this file.
+
+## [0.3.5]  - Mar 18, 2019
+* Added label selector for KubeXray ingress
+
+## [0.3.4] - Mar 15, 2019
+* Revert securityContext change that was causing issues
+
+## [0.3.3] - Mar 13, 2019
+* Move securityContext to container level
+
+## [0.3.2] - Dec 12, 2018
+* Fixed typo
+
+## [0.3.1] - Dec 12, 2018
+* Add `whitelistNamespaces` to all policies and fix some errors in [README.md]
+
+## [0.2.10] - Dec 7, 2018
+* Initial chart release
