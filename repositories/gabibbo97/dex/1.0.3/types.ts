68365430cadc25f8b7470fccac4ad89c084b1e17
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:47 -0500

    Add gabibbo97/dex chart

---
+++ repositories/gabibbo97/dex/1.0.3/types.ts
@@ -0,0 +1,27 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  connectors?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    host?: any
+    tls?: {
+      secretName?: any
+    }
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  staticClients?: any
+  staticPasswords?: any
+  tolerations?: any
+}
+
