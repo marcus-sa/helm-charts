08415a2fdad86eb191a6c89e26c49249630ab444
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:47 -0500

    Add incubator/spark chart

---
+++ repositories/incubator/spark/0.1.0/types.ts
@@ -0,0 +1,41 @@
+// Automatically generated
+
+export interface ChartValues {
+  Master?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    Image?: any
+    ImageTag?: any
+    Memory?: any
+    Name?: any
+    Replicas?: any
+    ServicePort?: any
+  }
+  WebUi?: {
+    ContainerPort?: any
+    Name?: any
+    ServicePort?: any
+  }
+  Worker?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    Image?: any
+    ImageTag?: any
+    Memory?: any
+    Name?: any
+    Replicas?: any
+  }
+  Zeppelin?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    Image?: any
+    ImageTag?: any
+    Name?: any
+    Replicas?: any
+    ServicePort?: any
+  }
+}
+
---
+++ repositories/incubator/spark/0.1.1/types.ts
@@ -0,0 +1,44 @@
+// Automatically generated
+
+export interface ChartValues {
+  Master?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    DaemonMemory?: any
+    Image?: any
+    ImageTag?: any
+    Memory?: any
+    Name?: any
+    Replicas?: any
+    ServicePort?: any
+  }
+  WebUi?: {
+    ContainerPort?: any
+    Name?: any
+    ServicePort?: any
+  }
+  Worker?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    DaemonMemory?: any
+    ExecutorMemory?: any
+    Image?: any
+    ImageTag?: any
+    Memory?: any
+    Name?: any
+    Replicas?: any
+  }
+  Zeppelin?: {
+    Component?: any
+    ContainerPort?: any
+    Cpu?: any
+    Image?: any
+    ImageTag?: any
+    Name?: any
+    Replicas?: any
+    ServicePort?: any
+  }
+}
+
