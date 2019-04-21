1fef6b62c3875bb0f4a7eca8beb94401a5d1f7eb
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:34:04 -0500

    Add stable/sensu chart

---
+++ repositories/stable/sensu/0.1.1/types.ts
@@ -0,0 +1,26 @@
+// Automatically generated
+
+export interface ChartValues {
+  REDIS_AUTO_RECONNECT?: any
+  REDIS_DB?: any
+  REDIS_PORT?: any
+  REDIS_RECONNECT_ON_ERROR?: any
+  api?: {
+    resources?: any
+  }
+  deis?: {
+    domains?: any
+    routable?: any
+  }
+  httpPort?: any
+  image?: any
+  imageTag?: any
+  nameOverride?: any
+  pullPolicy?: any
+  replicaCount?: any
+  server?: {
+    resources?: any
+  }
+  serviceType?: any
+}
+
