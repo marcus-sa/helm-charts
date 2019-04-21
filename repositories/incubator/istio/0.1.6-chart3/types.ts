a0f2c304e69dae991f862e7bec146e4283a738a5
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:19 -0500

    Add incubator/istio chart

---
+++ repositories/incubator/istio/0.1.6-chart3/types.ts
@@ -0,0 +1,157 @@
+// Automatically generated
+
+export interface ChartValues {
+  addons?: {
+    grafana?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        replicas?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+    prometheus?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        imageTag?: any
+        name?: any
+        replicas?: any
+        resources?: any
+      }
+      enabled?: any
+      service?: {
+        annotations?: any
+        externalPort?: any
+      }
+    }
+    servicegraph?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        imageTag?: any
+        name?: any
+        replicas?: any
+        resources?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+    zipkin?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        imageTag?: any
+        name?: any
+        replicas?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+  }
+  auth?: {
+    enabled?: any
+  }
+  ca?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+  }
+  egress?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      externalHttpPort?: any
+      type?: any
+    }
+  }
+  ingress?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      externalHttpPort?: any
+      externalHttpsPort?: any
+      type?: any
+    }
+  }
+  istioRelease?: any
+  mixer?: {
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      annotations?: any
+      externalConfigApiPort?: any
+      externalPrometheusPort?: any
+      externalTcpPort?: any
+      type?: any
+    }
+  }
+  nameOverride?: any
+  pilot?: {
+    customConfigMap?: any
+    deployment?: {
+      annotations?: any
+      apiserver?: {
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        resources?: any
+      }
+      discovery?: {
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        resources?: any
+      }
+      replicas?: any
+    }
+    name?: any
+    service?: {
+      externalHttpApiServer?: any
+      externalHttpDiscovery?: any
+      type?: any
+    }
+  }
+  rbac?: {
+    apiVersion?: any
+    install?: any
+  }
+}
+
---
+++ repositories/incubator/istio/0.2.7-chart2/types.ts
@@ -0,0 +1,173 @@
+// Automatically generated
+
+export interface ChartValues {
+  addons?: {
+    grafana?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        replicas?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+    prometheus?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        imageTag?: any
+        name?: any
+        replicas?: any
+        resources?: any
+      }
+      enabled?: any
+      service?: {
+        annotations?: any
+        externalPort?: any
+        type?: any
+      }
+    }
+    servicegraph?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        replicas?: any
+        resources?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+    zipkin?: {
+      deployment?: {
+        annotations?: any
+        image?: any
+        imagePullPolicy?: any
+        imageTag?: any
+        name?: any
+        replicas?: any
+      }
+      enabled?: any
+      service?: {
+        externalPort?: any
+        type?: any
+      }
+    }
+  }
+  auth?: {
+    enabled?: any
+  }
+  ca?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+  }
+  egress?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      externalHttpPort?: any
+      type?: any
+    }
+  }
+  ingress?: {
+    annotations?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      externalHttpPort?: any
+      externalHttpsPort?: any
+      type?: any
+    }
+  }
+  initializer?: {
+    annotations?: any
+    customConfigMap?: any
+    deployment?: {
+      image?: any
+      imagePullPolicy?: any
+      initImage?: any
+      name?: any
+      proxyImage?: any
+      replicas?: any
+      resources?: any
+    }
+    enabled?: any
+  }
+  istio?: {
+    install?: any
+    release?: any
+  }
+  mixer?: {
+    customConfigMap?: any
+    deployment?: {
+      annotations?: any
+      image?: any
+      imagePullPolicy?: any
+      name?: any
+      replicas?: any
+      resources?: any
+    }
+    service?: {
+      annotations?: any
+      externalConfigApiPort?: any
+      externalHttpHeathPort?: any
+      externalPrometheusPort?: any
+      externalStatsdPromPort?: any
+      externalStatsdUdpPort?: any
+      externalTcpPort?: any
+      type?: any
+    }
+  }
+  nameOverride?: any
+  pilot?: {
+    customConfigMap?: any
+    deployment?: {
+      annotations?: any
+      discovery?: {
+        image?: any
+        imagePullPolicy?: any
+        name?: any
+        resources?: any
+      }
+      replicas?: any
+    }
+    name?: any
+    service?: {
+      externalAdmissionWebhookServer?: any
+      externalHttpDiscovery?: any
+      type?: any
+    }
+  }
+  rbac?: {
+    apiVersion?: any
+    install?: any
+  }
+}
+
