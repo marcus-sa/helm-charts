0e756010d382087210ae91c75d4653c1e671a9f1
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:34:40 -0500

    Add stable/stash chart

---
+++ repositories/stable/stash/0.1.0/types.ts
@@ -0,0 +1,21 @@
+// Automatically generated
+
+export interface ChartValues {
+  nameOverride?: any
+  operator?: {
+    image?: any
+    pullPolicy?: any
+    tag?: any
+  }
+  pushgateway?: {
+    image?: any
+    pullPolicy?: any
+    tag?: any
+  }
+  rbac?: {
+    apiVersion?: any
+    install?: any
+  }
+  replicaCount?: any
+}
+
---
+++ repositories/stable/stash/0.5.0/types.ts
@@ -0,0 +1,33 @@
+// Automatically generated
+
+export interface ChartValues {
+  apiserver?: {
+    ca?: any
+    enableMutatingWebhook?: any
+    enableValidatingWebhook?: any
+    groupPriorityMinimum?: any
+    versionPriority?: any
+  }
+  criticalAddon?: any
+  dockerRegistry?: any
+  imagePullPolicy?: any
+  imagePullSecrets?: any
+  nameOverride?: any
+  operator?: {
+    image?: any
+    tag?: any
+  }
+  pushgateway?: {
+    image?: any
+    tag?: any
+  }
+  rbac?: {
+    create?: any
+  }
+  replicaCount?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+}
+
