1f75c62219d4c7159d5736a55102839fbfadb23f
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:01 -0500

    Add bitnami/parse chart

---
+++ repositories/bitnami/parse/0.3.9/types.ts
@@ -0,0 +1,41 @@
+// Automatically generated
+
+export interface ChartValues {
+  dashboard?: {
+    appName?: any
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    password?: any
+    resources?: any
+    username?: any
+  }
+  loadBalancerIP?: any
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    size?: any
+    storageClass?: any
+  }
+  server?: {
+    appId?: any
+    host?: any
+    image?: {
+      pullPolicy?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    masterKey?: any
+    mountPath?: any
+    port?: any
+    resources?: any
+  }
+  serviceType?: any
+}
+
---
+++ repositories/bitnami/parse/6.2.3/types.ts
@@ -0,0 +1,78 @@
+// Automatically generated
+
+export interface ChartValues {
+  dashboard?: {
+    appName?: any
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      pullSecrets?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    password?: any
+    resources?: any
+    securityContext?: {
+      enabled?: any
+      fsGroup?: any
+      runAsUser?: any
+    }
+    username?: any
+  }
+  global?: {
+    imagePullSecrets?: any
+    imageRegistry?: any
+  }
+  ingress?: {
+    annotations?: any
+    certManager?: any
+    dashboard?: {
+      hosts?: any
+    }
+    enabled?: any
+    server?: {
+      hosts?: any
+    }
+  }
+  mongodb?: {
+    usePassword?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    size?: any
+    storageClass?: any
+  }
+  server?: {
+    appId?: any
+    host?: any
+    image?: {
+      pullPolicy?: any
+      pullSecrets?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    masterKey?: any
+    mountPath?: any
+    port?: any
+    resources?: any
+    securityContext?: {
+      enabled?: any
+      fsGroup?: any
+      runAsUser?: any
+    }
+  }
+  service?: {
+    externalTrafficPolicy?: any
+    loadBalancerIP?: any
+    nodePorts?: {
+      http?: any
+    }
+    port?: any
+    type?: any
+  }
+}
+
