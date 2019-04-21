afe6f788d2b7c230085af17f06998b4aaa596d86
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:32 -0500

    Add stable/distributed-tensorflow chart

---
+++ repositories/stable/distributed-tensorflow/0.1.1/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  hyperparams?: {
+    batchsize?: any
+    learningrate?: any
+  }
+  nameOverride?: any
+  ps?: {
+    env?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    number?: any
+    podManagementPolicy?: any
+    port?: any
+    privileged?: any
+    resources?: any
+  }
+  volumeMounts?: any
+  volumes?: any
+  worker?: {
+    env?: any
+    gpuCount?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    number?: any
+    podManagementPolicy?: any
+    port?: any
+  }
+}
+
