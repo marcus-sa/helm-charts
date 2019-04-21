48d07dea3929d0953ac688994e5aed6f2d58e8d3
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:27 -0500

    Add ibm-charts/ibm-galera-mariadb-dev chart

---
+++ repositories/ibm-charts/ibm-galera-mariadb-dev/1.0.1/types.ts
@@ -0,0 +1,37 @@
+// Automatically generated
+
+export interface ChartValues {
+  arch?: any
+  dataPVC?: {
+    name?: any
+    selector?: {
+      label?: any
+      value?: any
+    }
+    size?: any
+    storageClassName?: any
+  }
+  dnsnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  mariadb?: {
+    configMapName?: any
+    database?: any
+    password?: any
+    rootPassword?: any
+    user?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    enabled?: any
+    useDynamicProvisioning?: any
+  }
+  replicas?: {
+    replicaCount?: any
+  }
+  resources?: any
+}
+
