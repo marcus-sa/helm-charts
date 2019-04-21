bb5e2413bd55950050b8557a37a31684984841cf
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:48 -0500

    Add stable/ethereum chart

---
+++ repositories/stable/ethereum/0.1.1/types.ts
@@ -0,0 +1,49 @@
+// Automatically generated
+
+export interface ChartValues {
+  bootnode?: {
+    image?: {
+      repository?: any
+      tag?: any
+    }
+  }
+  ethstats?: {
+    image?: {
+      repository?: any
+      tag?: any
+    }
+    service?: {
+      type?: any
+    }
+    webSocketSecret?: any
+  }
+  fullnameOverride?: any
+  geth?: {
+    account?: {
+      address?: any
+      privateKey?: any
+      secret?: any
+    }
+    genesis?: {
+      difficulty?: any
+      gasLimit?: any
+      networkId?: any
+    }
+    image?: {
+      repository?: any
+      tag?: any
+    }
+    miner?: {
+      replicaCount?: any
+    }
+    tx?: {
+      replicaCount?: any
+      service?: {
+        type?: any
+      }
+    }
+  }
+  imagePullPolicy?: any
+  nameOverride?: any
+}
+
