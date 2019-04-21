a87f30d70f25d01641893bd94ef00cd950a0b998
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:25:42 -0500

    Add cic/citrix-k8s-ingress-controller chart

---
+++ repositories/cic/citrix-k8s-ingress-controller/1.1.1/types.ts
@@ -0,0 +1,32 @@
+// Automatically generated
+
+export interface ChartValues {
+  exporter?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    ports?: {
+      containerPort?: any
+    }
+    require?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingressClass?: any
+  kubernetesURL?: any
+  license?: {
+    accept?: any
+  }
+  logLevel?: any
+  loginFileName?: any
+  nodeWatch?: any
+  nsIP?: any
+  nsPort?: any
+  nsProtocol?: any
+}
+
