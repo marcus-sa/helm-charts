c08089c14544fc969c3c9478feb653237091b8d3
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:37 -0500

    Add incubator/mongodb chart

---
+++ repositories/incubator/mongodb/0.1.0/types.ts
@@ -0,0 +1,18 @@
+// Automatically generated
+
+export interface ChartValues {
+  Component?: any
+  Cpu?: any
+  Image?: any
+  ImagePullPolicy?: any
+  ImageTag?: any
+  InstallImage?: any
+  InstallImageTag?: any
+  Memory?: any
+  Name?: any
+  PeerPort?: any
+  Replicas?: any
+  Storage?: any
+  StorageClass?: any
+}
+
