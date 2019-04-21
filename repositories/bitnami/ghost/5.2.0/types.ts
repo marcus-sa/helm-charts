6af507114bad8b206084c16dc373c10bf73b44bb
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:24:08 -0500

    Add bitnami/ghost chart

---
+++ repositories/bitnami/ghost/5.2.0/types.ts
@@ -0,0 +1,67 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  fullnameOverride?: any
+  ghostBlogTitle?: any
+  ghostEmail?: any
+  ghostHost?: any
+  ghostLoadBalancerIP?: any
+  ghostPassword?: any
+  ghostPath?: any
+  ghostPort?: any
+  ghostUsername?: any
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    enabled?: any
+    hosts?: any
+  }
+  mariadb?: {
+    db?: {
+      name?: any
+      user?: any
+    }
+    enabled?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    path?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  securityContext?: {
+    enabled?: any
+    fsGroup?: any
+    runAsUser?: any
+  }
+  serviceType?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpService?: any
+  smtpUser?: any
+  volumePermissions?: {
+    image?: {
+      name?: any
+      pullPolicy?: any
+      tag?: any
+    }
+  }
+}
+
---
+++ repositories/bitnami/ghost/6.2.3/types.ts
@@ -0,0 +1,82 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  fullnameOverride?: any
+  ghostBlogTitle?: any
+  ghostEmail?: any
+  ghostHost?: any
+  ghostPassword?: any
+  ghostPath?: any
+  ghostProtocol?: any
+  ghostUsername?: any
+  global?: {
+    imageRegistry?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    certManager?: any
+    enabled?: any
+    hosts?: any
+  }
+  mariadb?: {
+    db?: {
+      name?: any
+      user?: any
+    }
+    enabled?: any
+    mariadbRootPassword?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    path?: any
+    size?: any
+    storageClass?: any
+  }
+  resources?: any
+  securityContext?: {
+    enabled?: any
+    fsGroup?: any
+    runAsUser?: any
+  }
+  service?: {
+    externalTrafficPolicy?: any
+    loadBalancerIP?: any
+    nodePorts?: {
+      http?: any
+    }
+    port?: any
+    type?: any
+  }
+  smtpFromAddress?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpService?: any
+  smtpUser?: any
+  volumePermissions?: {
+    image?: {
+      pullPolicy?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+  }
+}
+
