16ee2425b794bc79f13cf8d6519aaa52fc61b1d4
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:16 -0500

    Add stable/hazelcast-jet chart

---
+++ repositories/stable/hazelcast-jet/1.0.0/types.ts
@@ -0,0 +1,55 @@
+// Automatically generated
+
+export interface ChartValues {
+  cluster?: {
+    memberCount?: any
+  }
+  fullnameOverride?: any
+  gracefulShutdown?: {
+    enabled?: any
+    maxWaitSeconds?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    repository?: any
+    tag?: any
+  }
+  jet?: {
+    configurationFiles?: any
+    javaOpts?: any
+    rest?: any
+  }
+  livenessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+  }
+  readinessProbe?: {
+    enabled?: any
+    failureThreshold?: any
+    initialDelaySeconds?: any
+    periodSeconds?: any
+    successThreshold?: any
+    timeoutSeconds?: any
+  }
+  resources?: any
+  service?: {
+    clusterIP?: any
+    port?: any
+    type?: any
+  }
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+}
+
