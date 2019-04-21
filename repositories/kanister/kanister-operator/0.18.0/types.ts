87042f6e24699a3e2fed80c0e0e5a1b5eb5473a0
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:23 -0500

    Add kanister/kanister-operator chart

---
+++ repositories/kanister/kanister-operator/0.18.0/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+}
+
