54377e0300091f63e7e3c8a4b24bf1e4b0fec2f7
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:06 -0500

    Add stable/auditbeat chart

---
+++ repositories/stable/auditbeat/0.4.2/types.ts
@@ -0,0 +1,31 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  config?: any
+  extraArgs?: any
+  extraVars?: any
+  extraVolumeMounts?: any
+  extraVolumes?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  plugins?: any
+  podAnnotations?: any
+  podLabels?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
