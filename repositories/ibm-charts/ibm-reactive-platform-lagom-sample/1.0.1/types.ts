a447a588effa562fcc41277f3814c1fce256f44e
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:26:46 -0500

    Add ibm-charts/ibm-reactive-platform-lagom-sample chart

---
+++ repositories/ibm-charts/ibm-reactive-platform-lagom-sample/1.0.1/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  alpineImage?: any
+  arch?: any
+  hostname?: any
+  image?: {
+    imageTags?: {
+      activity?: any
+      chirp?: any
+      friend?: any
+      frontend?: any
+    }
+    pullPolicy?: any
+    repository?: any
+  }
+  nameOverride?: any
+}
+
