0733f30781dad356575aa5ecd36a4d26626be325
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:52 -0500

    Add incubator/webpagetest-agent chart

---
+++ repositories/incubator/webpagetest-agent/0.1.3/types.ts
@@ -0,0 +1,20 @@
+// Automatically generated
+
+export interface ChartValues {
+  agent?: {
+    key?: any
+    location?: any
+    server?: any
+    shaper?: any
+  }
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+}
+
