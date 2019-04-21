a4c5d2968fe40dd99febbe4f5b1fe671850177d5
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:55 -0500

    Add rimusz/nfs-client-provisioner chart

---
+++ repositories/rimusz/nfs-client-provisioner/0.1.4/types.ts
@@ -0,0 +1,30 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nfs?: {
+    path?: any
+    server?: any
+  }
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+    serviceAccountName?: any
+  }
+  replicaCount?: any
+  storageClass?: {
+    create?: any
+    name?: any
+    provisionerName?: any
+    reclaimPolicy?: any
+  }
+  tolerations?: any
+}
+
