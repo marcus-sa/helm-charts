52cca7b09a3990469034e1efbeaa0f7886906767
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:36 -0500

    Add banzaicloud-stable/banzaicloud-docs chart

---
+++ repositories/banzaicloud-stable/banzaicloud-docs/0.1.1/types.ts
@@ -0,0 +1,30 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  deploymentAnnotations?: any
+  deploymentLabels?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    port?: any
+    type?: any
+  }
+  tolerations?: any
+}
+
