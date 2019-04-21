917c5e70af69380785ce920247232396fad27942
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:18 -0500

    Add stable/helm-exporter chart

---
+++ repositories/stable/helm-exporter/0.1.0/types.ts
@@ -0,0 +1,25 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+    serviceAccountName?: any
+  }
+  replicaCount?: any
+  resources?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+}
+
---
+++ repositories/stable/helm-exporter/0.2.0/types.ts
@@ -0,0 +1,31 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    create?: any
+    serviceAccountName?: any
+  }
+  replicaCount?: any
+  resources?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  serviceMonitor?: {
+    additionalLabels?: any
+    create?: any
+    interval?: any
+    namespace?: any
+  }
+  tolerations?: any
+}
+
