7004ab1498a3a5786555dff28456a28e5e07ef40
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:28:36 -0500

    Add loki/promtail chart

---
+++ repositories/loki/promtail/0.6.2/types.ts
@@ -0,0 +1,41 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  annotations?: any
+  config?: any
+  deploymentStrategy?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: any
+  loki?: {
+    fullnameOverride?: any
+    nameOverride?: any
+    serviceName?: any
+    servicePort?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  port?: any
+  priorityClassName?: any
+  rbac?: {
+    create?: any
+    pspEnabled?: any
+  }
+  readinessProbe?: any
+  resources?: any
+  securityContext?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  tolerations?: any
+  volumeMounts?: any
+  volumes?: any
+}
+
