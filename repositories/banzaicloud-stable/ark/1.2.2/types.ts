b06519498db5fd87ca927a5c4916ce9b2f865871
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:36 -0500

    Add banzaicloud-stable/ark chart

---
+++ repositories/banzaicloud-stable/ark/1.2.2/types.ts
@@ -0,0 +1,51 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  configuration?: {
+    backupStorageProvider?: {
+      bucket?: any
+      name?: any
+    }
+    persistentVolumeProvider?: {
+      name?: any
+    }
+  }
+  credentials?: {
+    existingSecret?: any
+    secretContents?: any
+    useSecret?: any
+  }
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  kubectl?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  rbac?: {
+    create?: any
+  }
+  resources?: any
+  serviceAccount?: {
+    hook?: {
+      create?: any
+      name?: any
+    }
+    server?: {
+      create?: any
+      name?: any
+    }
+  }
+  tolerations?: any
+}
+
