e3caa266d012470c27f8ffde864da6bc17f06592
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:49 -0500

    Add stable/express-gateway chart

---
+++ repositories/stable/express-gateway/1.0.1/types.ts
@@ -0,0 +1,50 @@
+// Automatically generated
+
+export interface ChartValues {
+  accessTokens?: any
+  admin?: {
+    annotations?: any
+    containerPort?: any
+    hostname?: any
+    loadBalancerIP?: any
+    nodePort?: any
+    servicePort?: any
+    type?: any
+  }
+  affinity?: any
+  authorizationCodes?: any
+  crypto?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  livenessProbe?: any
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  proxy?: {
+    annotations?: any
+    containerPort?: any
+    https?: any
+    loadBalancerIP?: any
+    nodePort?: any
+    servicePort?: any
+    tls?: any
+    type?: any
+  }
+  readinessProbe?: any
+  redis?: {
+    nameOverride?: any
+  }
+  refreshTokens?: any
+  replicaCount?: any
+  resources?: any
+  session?: any
+  storage?: {
+    emulate?: any
+    namespace?: any
+  }
+  tolerations?: any
+}
+
