7e66c12f98c9bf8e8982aa65317ecc768d5528e2
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:00 -0500

    Add incubator/cassandra-reaper chart

---
+++ repositories/incubator/cassandra-reaper/0.2.0/types.ts
@@ -0,0 +1,34 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  env?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    basicAuth?: {
+      enabled?: any
+      name?: any
+      secret?: any
+    }
+    enabled?: any
+    hosts?: any
+    labels?: any
+    path?: any
+    tls?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    type?: any
+  }
+  tolerations?: any
+}
+
