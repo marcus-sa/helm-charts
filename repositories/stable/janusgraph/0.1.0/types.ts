4a11c3544b24e63a59c23296c971b9e947fc12b6
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:31 -0500

    Add stable/janusgraph chart

---
+++ repositories/stable/janusgraph/0.1.0/types.ts
@@ -0,0 +1,28 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  persistence?: {
+    accessMode?: any
+    enabled?: any
+    existingClaim?: any
+    path?: any
+    size?: any
+    storageClass?: any
+  }
+  properties?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    port?: any
+    serviceAnnotations?: any
+    type?: any
+  }
+}
+
