52083f02ba8f3bd3b91fdff4acaf8e658590b4fd
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:29:33 -0500

    Add stable/dmarc2logstash chart

---
+++ repositories/stable/dmarc2logstash/1.0.0/types.ts
@@ -0,0 +1,38 @@
+// Automatically generated
+
+export interface ChartValues {
+  dmarc2logstash?: {
+    affinity?: any
+    extraContainer?: any
+    extraEnvs?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    nodeSelector?: any
+    resources?: any
+    tolerations?: any
+  }
+  extraSpec?: any
+  filebeat?: {
+    extraContainer?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    logstash?: {
+      extraOutput?: any
+      host?: any
+      index?: any
+      port?: any
+      timeout?: any
+    }
+    resources?: any
+    sourceType?: any
+  }
+  fullnameOverride?: any
+  nameOverride?: any
+}
+
