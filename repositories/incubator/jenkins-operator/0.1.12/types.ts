29ce85aeb0bacdd129adeffdf138c30d8dba0772
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:27:25 -0500

    Add incubator/jenkins-operator chart

---
+++ repositories/incubator/jenkins-operator/0.1.12/types.ts
@@ -0,0 +1,21 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  args?: any
+  chartCrds?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  keepCRDs?: any
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: any
+  replicaCount?: any
+  resources?: any
+  tolerations?: any
+}
+
