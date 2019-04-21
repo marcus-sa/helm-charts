09e7e3dbe9129fcdc56731502771a330b2c6828a
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:43 -0500

    Add banzaicloud-stable/mysql-operator chart

---
+++ repositories/banzaicloud-stable/mysql-operator/0.1.0/types.ts
@@ -0,0 +1,28 @@
+// Automatically generated
+
+export interface ChartValues {
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  mysql?: {
+    agent?: {
+      image?: any
+    }
+    server?: {
+      image?: any
+    }
+  }
+  nameOverride?: any
+  operator?: {
+    global?: any
+    namespace?: any
+    register_crd?: any
+  }
+  rbac?: {
+    enabled?: any
+  }
+}
+
