77501bfd8e4cd761141a715597fc7c514bdbc179
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:24:26 -0500

    Add bitnami/kubewatch chart

---
+++ repositories/bitnami/kubewatch/0.5.0/types.ts
@@ -0,0 +1,36 @@
+// Automatically generated
+
+export interface ChartValues {
+  affinity?: any
+  flock?: any
+  fullnameOverride?: any
+  global?: {
+    imageRegistry?: any
+  }
+  hipchat?: any
+  image?: {
+    pullPolicy?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  mattermost?: any
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  podLabels?: any
+  rbac?: {
+    create?: any
+  }
+  replicaCount?: any
+  resources?: any
+  resourcesToWatch?: any
+  serviceAccount?: {
+    create?: any
+    name?: any
+  }
+  slack?: any
+  tolerations?: any
+  webhook?: any
+}
+
