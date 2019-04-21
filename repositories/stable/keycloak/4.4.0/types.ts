52595ec9eed80ab63ea9c7d77db5db498eab73d3
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:30:46 -0500

    Add stable/keycloak chart

---
+++ repositories/stable/keycloak/4.4.0/types.ts
@@ -0,0 +1,87 @@
+// Automatically generated
+
+export interface ChartValues {
+  clusterDomain?: any
+  fullnameOverride?: any
+  init?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  keycloak?: {
+    affinity?: any
+    basepath?: any
+    cli?: any
+    extraArgs?: any
+    extraContainers?: any
+    extraEnv?: any
+    extraInitContainers?: any
+    extraPorts?: any
+    extraVolumeMounts?: any
+    extraVolumes?: any
+    hostAliases?: any
+    image?: {
+      pullPolicy?: any
+      pullSecrets?: any
+      repository?: any
+      tag?: any
+    }
+    ingress?: {
+      enabled?: any
+      hosts?: any
+      tls?: any
+    }
+    livenessProbe?: {
+      initialDelaySeconds?: any
+      timeoutSeconds?: any
+    }
+    nodeSelector?: any
+    password?: any
+    persistence?: {
+      dbHost?: any
+      dbName?: any
+      dbPassword?: any
+      dbPort?: any
+      dbUser?: any
+      dbVendor?: any
+      deployPostgres?: any
+      existingSecret?: any
+      existingSecretKey?: any
+    }
+    podAnnotations?: any
+    podDisruptionBudget?: any
+    preStartScript?: any
+    readinessProbe?: {
+      initialDelaySeconds?: any
+      timeoutSeconds?: any
+    }
+    replicas?: any
+    resources?: any
+    securityContext?: any
+    service?: {
+      jgroupsPort?: any
+      port?: any
+      type?: any
+    }
+    tolerations?: any
+    username?: any
+  }
+  nameOverride?: any
+  postgresql?: {
+    postgresDatabase?: any
+    postgresUser?: any
+    service?: {
+      port?: any
+    }
+  }
+  test?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+}
+
---
+++ repositories/stable/keycloak/4.6.2/types.ts
@@ -0,0 +1,90 @@
+// Automatically generated
+
+export interface ChartValues {
+  clusterDomain?: any
+  fullnameOverride?: any
+  init?: {
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+  keycloak?: {
+    affinity?: any
+    basepath?: any
+    cli?: any
+    extraArgs?: any
+    extraContainers?: any
+    extraEnv?: any
+    extraInitContainers?: any
+    extraPorts?: any
+    extraVolumeMounts?: any
+    extraVolumes?: any
+    hostAliases?: any
+    image?: {
+      pullPolicy?: any
+      pullSecrets?: any
+      repository?: any
+      tag?: any
+    }
+    ingress?: {
+      enabled?: any
+      hosts?: any
+      tls?: any
+    }
+    livenessProbe?: {
+      initialDelaySeconds?: any
+      timeoutSeconds?: any
+    }
+    nodeSelector?: any
+    password?: any
+    persistence?: {
+      dbHost?: any
+      dbName?: any
+      dbPassword?: any
+      dbPort?: any
+      dbUser?: any
+      dbVendor?: any
+      deployPostgres?: any
+      existingSecret?: any
+      existingSecretKey?: any
+    }
+    podAnnotations?: any
+    podDisruptionBudget?: any
+    podLabels?: any
+    preStartScript?: any
+    priorityClassName?: any
+    readinessProbe?: {
+      initialDelaySeconds?: any
+      timeoutSeconds?: any
+    }
+    replicas?: any
+    resources?: any
+    securityContext?: any
+    service?: {
+      jgroupsPort?: any
+      port?: any
+      type?: any
+    }
+    tolerations?: any
+    username?: any
+  }
+  nameOverride?: any
+  postgresql?: {
+    postgresDatabase?: any
+    postgresUser?: any
+    service?: {
+      port?: any
+    }
+  }
+  test?: {
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+  }
+}
+
