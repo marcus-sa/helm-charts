c9e6b74d5d62aa606eb22f3df53697c74162c6fa
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:44 -0500

    Add banzaicloud-stable/pipeline chart

---
+++ repositories/banzaicloud-stable/pipeline/0.2.10/types.ts
@@ -0,0 +1,135 @@
+// Automatically generated
+
+export interface ChartValues {
+  anchore?: {
+    secretKey?: any
+    secretName?: any
+    serviceEndpoint?: any
+    username?: any
+  }
+  cache?: {
+    persistence?: {
+      accessMode?: any
+      enabled?: any
+      existingClaim?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  cloudsql?: {
+    dbName?: any
+    dbUserName?: any
+    dbUserPass?: any
+    enabled?: any
+    image?: {
+      repository?: any
+      tag?: any
+    }
+    instance?: any
+  }
+  configuration?: {
+    anchore?: {
+      enabled?: any
+    }
+    auth?: {
+      clientid?: any
+      clientsecret?: any
+      tokenSigningKey?: any
+    }
+    cert?: {
+      ca?: {
+        cert?: any
+        key?: any
+      }
+      path?: any
+      source?: any
+    }
+    github?: {
+      token?: any
+    }
+    metrics?: {
+      enabled?: any
+      port?: any
+    }
+    monitor?: {
+      enabled?: any
+    }
+    pipelineBasepath?: any
+  }
+  deploymentAnnotations?: any
+  deploymentLabels?: any
+  env?: any
+  fullnameOverride?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  ingress?: {
+    annotations?: any
+    enabled?: any
+    hosts?: any
+  }
+  metrics?: {
+    alertRulesEnabled?: any
+    enabled?: any
+    port?: any
+    serviceMonitor?: {
+      additionalLabels?: any
+      enabled?: any
+      relabellings?: any
+    }
+  }
+  mysql?: {
+    enabled?: any
+    externalEndpoint?: any
+    fullnameOverride?: any
+    mysqlDatabase?: any
+    mysqlPassword?: any
+    mysqlUser?: any
+    nameOverride?: any
+  }
+  nameOverride?: any
+  replicaCount?: any
+  resources?: any
+  service?: {
+    externalPort?: any
+    internalPort?: any
+    name?: any
+    tls?: any
+    type?: any
+  }
+  serviceInternal?: {
+    externalPort?: any
+    internalPort?: any
+    name?: any
+    tls?: any
+    type?: any
+  }
+  statestore?: {
+    persistence?: {
+      accessMode?: any
+      enabled?: any
+      existingClaim?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  strategy?: any
+  vault?: {
+    serviceAddress?: any
+    tlsSecret?: any
+  }
+  worker?: {
+    deploymentAnnotations?: any
+    deploymentLabels?: any
+    image?: {
+      pullPolicy?: any
+      repository?: any
+      tag?: any
+    }
+    replicaCount?: any
+    strategy?: any
+  }
+}
+
