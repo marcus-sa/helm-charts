5b0a3c3a382aaa7902c7bb79a415ed5488f9cc7d
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:24:50 -0500

    Add bitnami/opencart chart

---
+++ repositories/bitnami/opencart/3.2.0/types.ts
@@ -0,0 +1,68 @@
+// Automatically generated
+
+export interface ChartValues {
+  allowEmptyPassword?: any
+  externalDatabase?: {
+    database?: any
+    host?: any
+    password?: any
+    port?: any
+    user?: any
+  }
+  global?: {
+    imageRegistry?: any
+  }
+  image?: {
+    pullPolicy?: any
+    pullSecrets?: any
+    registry?: any
+    repository?: any
+    tag?: any
+  }
+  mariadb?: {
+    db?: {
+      name?: any
+      user?: any
+    }
+    enabled?: any
+    mariadbRootPassword?: any
+  }
+  metrics?: {
+    enabled?: any
+    image?: {
+      pullPolicy?: any
+      registry?: any
+      repository?: any
+      tag?: any
+    }
+    podAnnotations?: any
+    resources?: any
+  }
+  nameOverride?: any
+  opencartEmail?: any
+  opencartLoadBalancerIP?: any
+  opencartPassword?: any
+  opencartUsername?: any
+  persistence?: {
+    apache?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+    enabled?: any
+    opencart?: {
+      accessMode?: any
+      size?: any
+      storageClass?: any
+    }
+  }
+  podAnnotations?: any
+  resources?: any
+  serviceType?: any
+  smtpHost?: any
+  smtpPassword?: any
+  smtpPort?: any
+  smtpProtocol?: any
+  smtpUser?: any
+}
+
