c130d36a1efa5d9d707d86f183b6ed4a8152b6a1
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:34:26 -0500

    Add stable/spartakus chart

---
+++ repositories/stable/spartakus/1.0.0/types.ts
@@ -0,0 +1,10 @@
+// Automatically generated
+
+export interface ChartValues {
+  image?: any
+  imagePullPolicy?: any
+  nameOverride?: any
+  resources?: any
+  uuid?: any
+}
+
---
+++ repositories/stable/spartakus/1.1.1/types.ts
@@ -0,0 +1,17 @@
+// Automatically generated
+
+export interface ChartValues {
+  extraArgs?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  podAnnotations?: any
+  replicaCount?: any
+  resources?: any
+  uuid?: any
+}
+
