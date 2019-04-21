2ee12b55bc011d8b148b82e4f3685e405e04340a
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-04-21 13:23:38 -0500

    Add banzaicloud-stable/etcd-operator chart

---
+++ repositories/banzaicloud-stable/etcd-operator/0.1.1/types.ts
@@ -0,0 +1,19 @@
+// Automatically generated
+
+export interface ChartValues {
+  commandArgs?: any
+  image?: {
+    pullPolicy?: any
+    repository?: any
+    tag?: any
+  }
+  nameOverride?: any
+  nodeSelector?: any
+  rbac?: {
+    apiVersion?: any
+    install?: any
+  }
+  replicaCount?: any
+  resources?: any
+}
+
